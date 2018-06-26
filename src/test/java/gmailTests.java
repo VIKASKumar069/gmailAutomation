d1bf4afd385b69d7b27074edab7208b3af3cd354
VIKAS Kumar <vikas_kumar@qainfotech.com>
2018-06-27 02:41:23 +0530

    first update

--- src/test/java/gmailTests.java
+++ src/test/java/gmailTests.java
@@ -3,13 +3,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+//import junit.framework.Assert;
+
 public class gmailTests {
    WebDriver driver;
    String url = "https://gmail.com";
+   String mailCountBefore, mailCountAfter, successfullSentmailMessage = "Your message has been sent. View message";
+   int sentmailsbefore;
+   
    
    @BeforeTest
    public void LaunchSignInPage() {
@@ -21,27 +27,42 @@ public class gmailTests {
    @Test(priority=0)
    public void loginGmail() {
 	   loginPage obj = new loginPage(driver);
-	   obj.login("kvikas0069", "01122782020");
+	   obj.login("kvikas96", "01122782020");
    }
    
    @Test(priority=1, enabled=true)
    public void unreadMails() {
 	   LoggedInPage obj = new LoggedInPage(driver);
-	   obj.unreadEmails();
+	   mailCountBefore = obj.unreadEmails();
    }
    
    @Test(priority=2)
    public void readFirstUnreadMail() {
 	   LoggedInPage obj = new LoggedInPage(driver);
 	   obj.readFirstUnreadEmail();
+	   System.out.println("After reading first unread mail");
+	   mailCountAfter = obj.unreadEmails();
+	   Assert.assertTrue((Integer.parseInt(mailCountAfter)+1) == Integer.parseInt(mailCountBefore));
    }
    
-   @Test(priority=3) 
-   public void updateUnreadMails() {
-	   System.out.println("After reading first unread mail");
+  /* @Test(priority=1)
+   public void sentMailBefore() {
 	   LoggedInPage obj = new LoggedInPage(driver);
-	   obj.unreadEmails();
+	   sentmailsbefore = obj.sentMails("before");
+	   System.out.println("Mails Before ="+sentmailsbefore);
    }
+   
+   @Test(priority=2)
+   public void sendMail() throws InterruptedException {
+	   LoggedInPage obj = new LoggedInPage(driver);
+	   Assert.assertEquals(obj.composeMail(), successfullSentmailMessage);
+	   //-- VALIDATING SENT MAIL THROUGH TABLE ---
+	   // obj.composeMail();
+	//   System.out.println(obj.sentMails("after"));
+	//   Assert.assertTrue( (sentmailsbefore+1) == obj.sentMails("after"));
+	   //-------------------------------------------
+
+   }*/
    
    @AfterTest
    public void close() throws InterruptedException {
